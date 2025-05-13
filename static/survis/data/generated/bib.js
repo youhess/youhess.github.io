21ee58fbfad44455fcb31c21c002ed7135accec0
youhess <1176305338@qq.com>
2025-05-13 21:03:31 +0100

    增加survis

---
+++ static/survis/data/generated/bib.js
@@ -0,0 +1,107 @@
+﻿const generatedBibEntries = {
+    "batson2019noise2self": {
+        "author": "Batson, Joshua and Royer, Loic",
+        "booktitle": "Proceedings of the 36th International Conference on Machine Learning",
+        "doi": "10.48550/arXiv.1901.11365",
+        "keywords": "type:self-supervised, blind-denoising, CNN, image-denoising",
+        "organization": "PMLR",
+        "pages": "524--533",
+        "title": "Noise2Self: Blind Denoising by Self-Supervision",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "huang2021neighbor2neighbor": {
+        "author": "Huang, Yijun and He, Junyuan and Sun, Deqing and Sunkavalli, Kalyan and Ramamoorthi, Ravi and Carr, Nathan and Xu, Zhengqi",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "doi": "10.1109/CVPR46437.2021.01454",
+        "keywords": "type:self-supervised, single-image, CNN, image-denoising",
+        "pages": "14776--14785",
+        "title": "Neighbor2Neighbor: Self-Supervised Denoising from Single Noisy Images",
+        "type": "inproceedings",
+        "year": "2021"
+    },
+    "krull2019noise2void": {
+        "author": "Krull, Alexander and Buchholz, Tim-Oliver and Jug, Florian",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "doi": "10.1109/CVPR.2019.00223",
+        "keywords": "type:self-supervised, blind-denoising, CNN, image-denoising",
+        "pages": "2129--2137",
+        "title": "Noise2Void - Learning Denoising from Single Noisy Images",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "lehtinen2018noise2noise": {
+        "author": "Lehtinen, Jaakko and Munkberg, Jacob and Hasselgren, Jon and Laine, Samuli and Karras, Tero and Aittala, Miika and Aila, Timo",
+        "booktitle": "Proceedings of the 35th International Conference on Machine Learning",
+        "doi": "10.48550/arXiv.1803.04189",
+        "keywords": "type:Self-supervised, noisy-pairs, CNN, image-denoising",
+        "organization": "PMLR",
+        "pages": "2965--2974",
+        "title": "Noise2Noise: Learning Image Restoration without Clean Data",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "liang2021swinir": {
+        "author": "Liang, Jingyun and Cao, Jiezhang and Sun, Guolei and Zhang, Kai and Van Gool, Luc and Timofte, Radu",
+        "booktitle": "Proceedings of the IEEE/CVF International Conference on Computer Vision Workshops",
+        "doi": "10.1109/ICCVW54120.2021.00210",
+        "keywords": "type:supervised, transformer, swin, image-denoising",
+        "pages": "1833--1844",
+        "title": "SwinIR: Image Restoration Using Swin Transformer",
+        "type": "inproceedings",
+        "year": "2021"
+    },
+    "quan2020self2self": {
+        "author": "Quan, Yuhui and Chen, Mingqin and Pang, Tongyao and Ji, Hui",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+        "doi": "10.1109/CVPR42600.2020.00196",
+        "keywords": "type:self-supervised, dropout, ensemble, image-denoising",
+        "pages": "1890--1898",
+        "title": "Self2Self With Dropout: Learning Self-Supervised Denoising From Single Image",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "ulyanov2018deep": {
+        "author": "Ulyanov, Dmitry and Vedaldi, Andrea and Lempitsky, Victor",
+        "booktitle": "Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition",
+        "doi": "10.1109/CVPR.2018.00984",
+        "keywords": "type:unsupervised, implicit-prior, CNN, image-denoising",
+        "pages": "9446--9454",
+        "title": "Deep Image Prior",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "yasarla2024sstgp": {
+        "author": "Yasarla, Rajeev and Patel, Vishal M.",
+        "doi": "10.48550/arXiv.2402.03629",
+        "journal": "arXiv preprint arXiv:2402.03629",
+        "keywords": "type:self-supervised, transformer, gaussian-process, image-denoising",
+        "title": "Self-Supervised Denoising Transformer with Gaussian Process",
+        "type": "article",
+        "year": "2024"
+    },
+    "zhang2017beyond": {
+        "author": "Zhang, Kai and Zuo, Wangmeng and Chen, Yunjin and Meng, Deyu and Zhang, Lei",
+        "doi": "10.1109/TIP.2017.2662206",
+        "journal": "IEEE Transactions on Image Processing",
+        "keywords": "type:supervised, residual, CNN, image-denoising",
+        "number": "7",
+        "pages": "3142--3155",
+        "title": "Beyond a Gaussian Denoiser: Residual Learning of Deep CNN for Image Denoising",
+        "type": "article",
+        "volume": "26",
+        "year": "2017"
+    },
+    "zhang2018ffdnet": {
+        "author": "Zhang, Kai and Zuo, Wangmeng and Zhang, Lei",
+        "doi": "10.1109/TIP.2018.2839891",
+        "journal": "IEEE Transactions on Image Processing",
+        "keywords": "type:supervised, fast-denoising, CNN, image-denoising",
+        "number": "9",
+        "pages": "4608--4622",
+        "title": "FFDNet: Toward a Fast and Flexible Solution for CNN-Based Image Denoising",
+        "type": "article",
+        "volume": "27",
+        "year": "2018"
+    }
+};
